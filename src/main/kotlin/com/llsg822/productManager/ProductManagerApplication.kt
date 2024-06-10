3825efe78fbfd5f82952ed6cf09495df3c339e5a
이승규 <llsg822@llsg822-MacBookPro.local>
2024-06-10 23:16:01 +0900

    init

---
+++ src/main/kotlin/com/llsg822/productManager/ProductManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.llsg822.productManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductManagerApplication>(*args)
+}
